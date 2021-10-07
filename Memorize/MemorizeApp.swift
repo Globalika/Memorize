30ff8553fde6ff407d691cc13b725e2aad31466c
Globalika <volodymyr.seredovych@gmail.com>
2021-10-07 12:43:05 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Volodymyr Seredovych on 07.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
